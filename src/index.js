a8010882c97227ce8d1c93b86d93aa2905d9086c
builde7b0b <ggives3@gmail.com>
2024-01-07 13:50:14 -0700

    update UI

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Modal from 'react-modal';
+
+// LIVE - PROD
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY_PROD);
+// Modal.setAppElement('#root'); // Replace '#root' with the appropriate element where your React app is mounted.
+
+// testing code 
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+Modal.setAppElement('#root');
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('Running in development mode');
+} else if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+        <Elements stripe={stripePromise}>
+
     <App />
+    </Elements>
   </React.StrictMode>
 );
 
